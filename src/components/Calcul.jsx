814cd7257db95f2cb6fccce018e47b9a834b4ab0
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-12 10:26:03 +0100

    solution

--- src/components/Calcul.jsx
+++ src/components/Calcul.jsx
@@ -1,21 +1,38 @@
 import React, { Component } from 'react'
 
 export default class Calcul extends Component {
+
+    state = {
+        capital: 0,
+        taux: 0,
+        duree: 0,
+        mensualité: 0
+    }
+
+    handleOnchange(e) {
+        const value = e.target.value;
+        const name = e.target.name;
+
+        this.setState({
+            [name]:  value
+        });
+    }
+
     render() {
         return (
             <div className='calcul'>
                 <div className='content'>
                     <div className='group'>
                         <label htmlFor='capital'>Capital emprunté :</label>
-                        <input type="number" name="capital" id="capital" />
+                        <input type="number" name="capital" id="capital" value={this.state.capital} onChange={this.handleOnchange} />
                     </div>
                     <div className='group'>
                         <label htmlFor='taux'>Taux d'interet :</label>
-                        <input type="number" name="taux" id="taux" />
+                        <input type="number" name="taux" id="taux" value={this.state.taux} onChange={this.handleOnchange}/>
                     </div>
                     <div className='group'>
                         <label htmlFor='duree'>Durée de remboursement :</label>
-                        <input type="number" name="duree" id="duree" />
+                        <input type="number" name="duree" id="duree" value={this.state.duree} onChange={this.handleOnchange}/>
                     </div>
                     <div className='btns'>
                         <button>Calculer</button>
@@ -23,7 +40,7 @@ export default class Calcul extends Component {
                     </div>
                     <div className='group'>
                         <label htmlFor='mens'>Mensualité :</label>
-                        <input type="number" name="mens" id="mens" readOnly />
+                        <input type="number" name="mensualité" id="mens" value={this.state.mensualité} onChange={this.handleOnchange} readOnly />
                     </div>
                 </div>
             </div>
